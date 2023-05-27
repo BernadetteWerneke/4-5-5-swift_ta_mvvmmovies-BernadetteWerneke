9c4df5ecf499129e1c52f3ec9cc5eb293accd39f
Bernadette Werneke <bernadettewerneke@gmail.com>
2023-05-27 10:38:48 +0200

    zweiter Versuch zu comitten...

--- MVVMMovies/View/MovieCard.swift
+++ MVVMMovies/View/MovieCard.swift
@@ -8,18 +8,36 @@
 import SwiftUI
 
 struct MovieCard: View {
-
+    var movie: Movie
+   
     
     var body: some View {
         
-
-        VStack{}
-
+        NavigationStack{
+                HStack{
+                    Image(movie.image)
+                    .resizable()
+                    .frame(width: 120, height: 200)
+                    VStack{
+                        Text(movie.title)
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundColor(.blue)
+                            .padding()
+                        VStack{
+                            ForEach(0..<movie.times.count){
+                                Text(movie.times[$0])
+                            }
+                        }
+                    }
+            }
+        }
     }
 }
+        
 
 struct MovieCard_Previews: PreviewProvider {
     static var previews: some View {
-      MovieCard()
+        MovieCard(movie: Movie.sharedMovie)
     }
 }
